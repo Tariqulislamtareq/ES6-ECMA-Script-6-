74db9f2753fbf5cf24aecbab48ddb183103fd8e9
Tariqul Islam <pathharatareq@gmail.com>
2019-04-19 04:39:20 +0530

    Add files via upload

---
+++ destructuringArray.js
@@ -0,0 +1,9 @@
+let arr =[1,2,.5,44,46,-1,"hello",556,];
+let[a,b,,,,,str,, d='deafult'] = arr;
+let [x, ...num] =arr;
+console.log(arr);
+console.log(a);
+console.log(b);
+console.log(num);
+console.log(str);
+console.log(d);
