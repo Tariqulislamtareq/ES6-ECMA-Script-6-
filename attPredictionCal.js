e0304a4a95af9da3ebeda963aeeb3ded07aa09e6
Tariqul Islam <pathharatareq@gmail.com>
2019-04-19 02:28:39 +0530

    Add files via upload

---
+++ attPredictionCal.js
@@ -0,0 +1,15 @@
+const calculateAttendence = (numberOfclassesPerWeek, numOfWeeksPerSem, totalAttClasses)=>{
+  const totalClasses = numberOfclassesPerWeek * numOfWeeksPerSem;
+  
+  return totalAttClasses / totalClasses;
+};
+console.log("*******Attendence Prediction Calculator*******");
+const percentage = calculateAttendence(30, 14, 400) * 100;
+console.log(percentage + " %");
+if(percentage >= 90){
+  console.log("Congratulation You Have Good Attendence Label");
+}else if(percetage >= 75){
+  console.log("You Are Allowed To Seat In The Exam");
+}else if(percentage < 75){
+  console.log("Sorry Better Luck For Next Semester");
+}
