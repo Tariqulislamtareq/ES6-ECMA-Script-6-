f6c61dee28e50df7f862d1dc8a93aef621f0fa27
Tariqul Islam <pathharatareq@gmail.com>
2019-04-20 00:09:41 +0530

    Inheritance

---
+++ inheritance.js
@@ -0,0 +1,22 @@
+class Person{
+    constructor(name){
+        this.name = name;
+    }
+    greet(){
+        console.log("My Name is: "+this.name + "mt age is: "+this.age);
+    }
+}
+class Tareq extends Person{
+    constructor(age){
+        super('Max');
+        this.age = age;
+    }
+   
+    greetTwice(){
+        this.greet();
+    }
+
+}
+let tareq = new Tareq(22);
+tareq.greet();
+tareq.greetTwice();
